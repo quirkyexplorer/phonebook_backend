9d41054faf3472b0fa0924365ed82025601c3a5a
quirkexplorer <quirkyexplorer@gmail.com>
2023-08-08 14:21:49 -0700

    added hello

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors()); // middleware that allows cross origin requests.
 app.use(express.static('build'))
 
 app.use(express.json());
+console.log("hello");
 
 app.use(morgan("tiny"));
 // app.use(morgan(":body", options)); // using a format string
